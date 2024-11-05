e41de3744ed4f2777a5d8dae6aef0d73fb52ac70
pescocofino <134400750+pescocofino@users.noreply.github.com>
2024-11-05 09:08:18 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,5 @@
-document.getElementById('formContato').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulário enviado com sucesso!');
+// Função para o envio de formulário (simulação de envio)
+document.getElementById("form-contato").addEventListener("submit", function(event) {
+    event.preventDefault();  // Previne o envio real para testar
+    alert("Mensagem enviada com sucesso!");
 });
